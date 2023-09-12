effac615d28531659a7d16f8a8d7c52d3daf9bc0
alextrack2013 <alextrack2013@gmail.com>
2023-09-12 19:47:04 +0200

    Revert "RMX1931: Drop mock power stats"
    This reverts commit a9da69c4d35fa84e3ba2d902bc16389c0cbaa4a4.

--- device.mk
+++ device.mk
@@ -356,7 +356,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
