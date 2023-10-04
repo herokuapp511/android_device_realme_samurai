96234d17d69c3aaa05349185a95f2e5aefda721d
Tim Zimmermann <tim@linux4.de>
2023-10-04 19:47:23 +0530

    samurai: Patch some blobs to load libprotobuf-cpp-full-3.9.1
    * Patch them to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1

--- device.mk
+++ device.mk
@@ -393,6 +393,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QMI Configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,12 @@ function blob_fixup() {
         vendor/lib64/hw/camera.qcom.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
+        odm/lib/mediadrm/libwvdrmengine.so | odm/lib64/libwvhidl.so | odm/lib64/mediadrm/libwvdrmengine.so |
+        vendor/bin/sensors.qti | vendor/lib/libssc.so | vendor/lib/libsnsdiaglog.so | vendor/lib/libsnsapi.so |
+        vendor/lib/sensors.ssc.so | vendor/lib/libsensorcal.so | vendor/lib64/libssc.so | vendor/lib64/libsnsdiaglog.so |
+        vendor/lib64/libsnsapi.so | vendor/lib64/sensors.ssc.so | vendor/lib64/libsensorcal.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
