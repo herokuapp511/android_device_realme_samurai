42d8558897159ca08b46fca336eb0f176d49773d
Zannatun Nayem <znayem830@gmail.com>
2023-12-31 21:14:30 +0600

    AfterLife Project

--- afterlife_samurai.mk
+++ afterlife_samurai.mk
@@ -6,15 +6,30 @@
 
 $(call inherit-product, device/realme/samurai/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AFTERLIFE stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+# AfterLife Flags
+AFTERLIFE_CORE := true
+AFTERLIFE_GAPPS := true
+USE_PIXEL_CHARGING =: true
+AFTERLIFE_MAINTAINER := nayem
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_SUPPORTS_TOUCHGESTURES := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := samurai
 PRODUCT_MANUFACTURER := realme
 PRODUCT_MODEL := realme X2 Pro
-PRODUCT_NAME := lineage_samurai
+PRODUCT_NAME := afterlife_samurai
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := "realme/RMX1931/RMX1931L1:11/RKQ1.200928.002/1625457120608:user/release-keys"
 
