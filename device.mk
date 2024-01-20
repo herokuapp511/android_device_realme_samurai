cd23f74dc0cff083a00194da5f60918641f50552
Zannatun Nayem <znayem830@gmail.com>
2024-01-20 10:33:16 +0600

    overlay arranged

--- device.mk
+++ device.mk
@@ -26,16 +26,14 @@ RELAX_USES_LIBRARY_CHECK := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigRes \
-    FrameworksRes \
-    SettingsProviderRes \
     SettingsRes \
     SimpleDeviceConfigCommon \
-    SystemUIRes \
     TelephonyRes \
     TetheringRes \
     WifiRes
